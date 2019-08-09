6d7882407fe18837d4ff9dfa1c58f89f7ccafaaa
Kees Meijer <keesie.meijer@gmail.com>
2019-08-09 16:06:46 +0200

    Add color picker

--- src/utils.js
+++ src/utils.js
@@ -1,32 +1,36 @@
+function isValidHex(hex) {
+	return /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/i.test(hex.trim());
+}
+
 function replaceAt(str, index, replacement) {
-  if (index > str.length - 1) {
-    return str;
-  }
-  return str.substr(0, index) + replacement + str.substr(index + 1);
+	if (index > str.length - 1) {
+		return str;
+	}
+	return str.substr(0, index) + replacement + str.substr(index + 1);
 }
 
 function stringVal(str, index) {
-  return parseInt(str.charAt(index), 10);
+	return parseInt(str.charAt(index), 10);
 }
 
 function getInputIntVal(id, defaultValue) {
-  const el = document.getElementById(id);
-  if (el) {
-    let el_value = parseInt(el.value, 10);
-    el_value = (0 < el_value) ? el_value : defaultValue;
-    el.value = el_value;
-    return el_value;
-  }
+	const el = document.getElementById(id);
+	if (el) {
+		let el_value = parseInt(el.value, 10);
+		el_value = (0 < el_value) ? el_value : defaultValue;
+		el.value = el_value;
+		return el_value;
+	}
 
-  el.value = defaultValue;
-  return defaultValue;
+	el.value = defaultValue;
+	return defaultValue;
 }
 
 function removeFromArray(arr, element) {
-  const index = arr.indexOf(element);
-  if (-1 !== index) {
-    arr.splice(index, 1);
-  }
+	const index = arr.indexOf(element);
+	if (-1 !== index) {
+		arr.splice(index, 1);
+	}
 }
 
 /**
@@ -34,10 +38,10 @@ function removeFromArray(arr, element) {
  * Using Durstenfeld shuffle algorithm.
  */
 function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    let temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
+	for (let i = array.length - 1; i > 0; i--) {
+		let j = Math.floor(Math.random() * (i + 1));
+		let temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
 }
