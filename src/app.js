cae5cc6f4f74bf9f62da6536ac83d3e1ff998d1c
Kees Meijer <keesie.meijer@gmail.com>
2021-02-02 15:03:35 +0100

    Add difference for maze and solved maze link text

--- src/app.js
+++ src/app.js
@@ -3,34 +3,34 @@ let mazeNodes = {};
 
 // Check if globals are defined
 if (typeof maxMaze === 'undefined') {
-	maxMaze = 0;
+    maxMaze = 0;
 }
 
 if (typeof maxSolve === 'undefined') {
-	maxSolve = 0;
+    maxSolve = 0;
 }
 
 if (typeof maxCanvas === 'undefined') {
-	maxCanvas = 0;
+    maxCanvas = 0;
 }
 
 if (typeof maxCanvasDimension === 'undefined') {
-	maxCanvasDimension = 0;
+    maxCanvasDimension = 0;
 }
 
 if (typeof maxWallsRemove === 'undefined') {
-	maxWallsRemove = 300;
+    maxWallsRemove = 300;
 }
 
 // Update remove max walls html
 const removeMaxWallsText = document.querySelector('.desc span');
 if (removeMaxWallsText) {
-	removeMaxWallsText.innerHTML = maxWallsRemove;
+    removeMaxWallsText.innerHTML = maxWallsRemove;
 }
 
 const removeWallsInput = document.getElementById('remove_walls');
 if (removeWallsInput) {
-	removeWallsInput.max = maxWallsRemove;
+    removeWallsInput.max = maxWallsRemove;
 }
 
 const download = document.getElementById("download");
@@ -38,100 +38,107 @@ download.addEventListener("click", downloadImage, false);
 download.setAttribute('download', 'maze.png');
 
 function initMaze() {
-	const settings = {
-		width: getInputIntVal('width', 20),
-		height: getInputIntVal('height', 20),
-		wallSize: getInputIntVal('wall-size', 10),
-		removeWalls: getInputIntVal('remove_walls', 0),
-		entryType: '',
-		bias: '',
-		color: '#000000',
-		backgroudColor: '#FFFFFF',
-		solveColor: '#cc3737',
+    download.setAttribute('download', 'maze.png');
+    download.innerHTML = 'download maze';
 
-		// restrictions
-		maxMaze: maxMaze,
-		maxCanvas: maxCanvas,
-		maxCanvasDimension: maxCanvasDimension,
-		maxSolve: maxSolve,
-		maxWallsRemove: maxWallsRemove,
-	}
+    const settings = {
+        width: getInputIntVal('width', 20),
+        height: getInputIntVal('height', 20),
+        wallSize: getInputIntVal('wall-size', 10),
+        removeWalls: getInputIntVal('remove_walls', 0),
+        entryType: '',
+        bias: '',
+        color: '#000000',
+        backgroudColor: '#FFFFFF',
+        solveColor: '#cc3737',
 
-	const colors = ['color', 'backgroundColor', 'solveColor'];
-	for (let i = 0; i < colors.length; i++) {
-		const colorInput = document.getElementById(colors[i]);
-		settings[colors[i]] = colorInput.value
-		if (!isValidHex(settings[colors[i]])) {
-			let defaultColor = colorInput.parentNode.dataset.default;
-			colorInput.value = defaultColor;
-			settings[colors[i]] = defaultColor;
-		}
+        // restrictions
+        maxMaze: maxMaze,
+        maxCanvas: maxCanvas,
+        maxCanvasDimension: maxCanvasDimension,
+        maxSolve: maxSolve,
+        maxWallsRemove: maxWallsRemove,
+    }
 
-		const colorSample = colorInput.parentNode.querySelector('.color-sample');
-		colorSample.style = 'background-color: ' + settings[colors[i]] + ';';
-	}
+    const colors = ['color', 'backgroundColor', 'solveColor'];
+    for (let i = 0; i < colors.length; i++) {
+        const colorInput = document.getElementById(colors[i]);
+        settings[colors[i]] = colorInput.value
+        if (!isValidHex(settings[colors[i]])) {
+            let defaultColor = colorInput.parentNode.dataset.default;
+            colorInput.value = defaultColor;
+            settings[colors[i]] = defaultColor;
+        }
 
-	if (settings['removeWalls'] > maxWallsRemove) {
-		settings['removeWalls'] = maxWallsRemove;
-		if (removeWallsInput) {
-			removeWallsInput.value = maxWallsRemove;
-		}
-	}
+        const colorSample = colorInput.parentNode.querySelector('.color-sample');
+        colorSample.style = 'background-color: ' + settings[colors[i]] + ';';
+    }
 
-	const entry = document.getElementById('entry');
-	if (entry) {
-		settings['entryType'] = entry.options[entry.selectedIndex].value;
-	}
+    if (settings['removeWalls'] > maxWallsRemove) {
+        settings['removeWalls'] = maxWallsRemove;
+        if (removeWallsInput) {
+            removeWallsInput.value = maxWallsRemove;
+        }
+    }
 
-	const bias = document.getElementById('bias');
-	if (bias) {
-		settings['bias'] = bias.options[bias.selectedIndex].value;
-	}
+    const entry = document.getElementById('entry');
+    if (entry) {
+        settings['entryType'] = entry.options[entry.selectedIndex].value;
+    }
 
-	const maze = new Maze(settings);
-	maze.generate();
-	maze.draw();
+    const bias = document.getElementById('bias');
+    if (bias) {
+        settings['bias'] = bias.options[bias.selectedIndex].value;
+    }
 
-	if (download && download.classList.contains('hide')) {
-		download.classList.toggle("hide");
-	}
+    const maze = new Maze(settings);
+    maze.generate();
+    maze.draw();
 
-	const solveButton = document.getElementById("solve");
-	if (solveButton && solveButton.classList.contains('hide')) {
-		solveButton.classList.toggle("hide");
-	}
+    if (download && download.classList.contains('hide')) {
+        download.classList.toggle("hide");
+    }
 
-	mazeNodes = {}
-	if (maze.matrix.length) {
-		mazeNodes = maze;
-	}
+    const solveButton = document.getElementById("solve");
+    if (solveButton && solveButton.classList.contains('hide')) {
+        solveButton.classList.toggle("hide");
+    }
 
-	location.href = "#";
-	location.href = "#generate";
+    mazeNodes = {}
+    if (maze.matrix.length) {
+        mazeNodes = maze;
+    }
+
+    location.href = "#";
+    location.href = "#generate";
 }
 
 function downloadImage(e) {
-	const image = document.getElementById('maze').toDataURL("image/png");
+    const image = document.getElementById('maze').toDataURL("image/png");
     image.replace("image/png", "image/octet-stream");
-	download.setAttribute("href", image);
+    download.setAttribute("href", image);
 }
 
 function initSolve() {
-	if ((typeof mazeNodes.matrix === 'undefined') || !mazeNodes.matrix.length) {
-		return;
-	}
+    const solveButton = document.getElementById("solve");
+    if (solveButton) {
+        solveButton.classList.toggle("hide");
+    }
 
-	const solver = new Solver(mazeNodes);
-	solver.solve();
-	if (mazeNodes.wallsRemoved) {
-		solver.drawAstarSolve();
-	} else {
-		solver.draw();
-	}
-	mazeNodes = {}
+    download.setAttribute('download', 'maze-solved.png');
+    download.innerHTML = 'download solved maze';
 
-	const solveButton = document.getElementById("solve");
-	if (solveButton) {
-		solveButton.classList.toggle("hide");
-	}
+    if ((typeof mazeNodes.matrix === 'undefined') || !mazeNodes.matrix.length) {
+        return;
+    }
+
+    const solver = new Solver(mazeNodes);
+    solver.solve();
+    if (mazeNodes.wallsRemoved) {
+        solver.drawAstarSolve();
+    } else {
+        solver.draw();
+    }
+
+    mazeNodes = {}
 }
